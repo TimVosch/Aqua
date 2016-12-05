333684c35d3aa2b9d9abce7e808c467b88a033a0
Jarimaster123 <jari.dedooij@hotmail.nl>
2016-12-05 11:55:26 +0100

    Add conductivity sensor INO

---
+++ Prototypes/Sensor_1_Conductivity_Sensor/Sensor_1_Conductivity_Sensor.ino
@@ -0,0 +1,17 @@
+#define SensorPin A0
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogRead(SensorPin);
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(2, HIGH);
+  delay(100);
+  Serial.println(analogRead(SensorPin));
+  digitalWrite(2, LOW);
+  delay(1000);
+}
